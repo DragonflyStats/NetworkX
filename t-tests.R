34959d339ebd824df40c69b2e85e73400ef51199
Dublin R <DublinRUserGroup@gmail.com>
2013-10-20 04:44:35 -0700

    Update and rename t-tests to t-tests.R

--- t-tests.R
+++ t-tests.R
@@ -1,4 +1,4 @@
-# R code to demonstrate implementation of various t-tespt procedure
+# R code to demonstrate implementation of various t-test procedure
 # 1) One sample t-test
 # 2) Two Indepedent sample t-test (equal variance assumed)
 # 3) Two Independent sample t-test (equal variance not assumed)
@@ -11,5 +11,12 @@
 X <- c(100,100,102,104,105,109,110,110)
 
 t.test(X,mu=100, conf.level = 0.99)
+
+################################################
+
+# Testing Equality of Variance
+
+Y <- c(120,112,103,114,111,107,113,99)
+var.test(X,Y)
 
 ################################################
