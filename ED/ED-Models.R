90cd5cd1be0b6bcb691335a01689e7fcf8f03f07
DragonflyStats <kobriendublin@gmail.com>
2015-10-27 02:34:33 +0000

    reload

--- ED/ED-Models.R
+++
@@ -1,4 +0,0 @@
-with(PlantGrowth, tapply(weight, group, mean))
-with(PlantGrowth, aov(weight ~ group)) -> aov.out
-summary.aov(aov.out)
-summary.lm(aov.out)
--- ED/EDwithR-DyeStuff.R
+++
@@ -1,6 +0,0 @@
-install.packages("lme4")
-library(lme4)
-summary(Dyestuff)
-Dyestuff
-
-##########################################################################
